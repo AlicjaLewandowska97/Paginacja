8937b85552759a22cc03e2701e0eed0d6750bf11
AlicjaLewandowska97 <ala.lew97@gmail.com>
2021-12-01 14:27:38 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import "./main.scss"
+
+import React, {useState, useEffect, useRef, useReducer} from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+const App = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root') )
