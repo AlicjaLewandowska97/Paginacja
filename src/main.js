a71c419ff3c6c3a5c807cddac6952104260702b7
AlicjaLewandowska97 <ala.lew97@gmail.com>
2021-12-01 14:46:38 +0100

    'node'

--- src/main.js
+++ src/main.js
@@ -7,9 +7,30 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
 
+    const [loading, setLoading] = useState(false)
+    const [list, setList] = useState([])
+    const [itemsPerPage, setItemsPerPage] = useState(3)
+    const [currentPage, setCurrentPage] = useState(1)
+    const allItems = useRef(0)
+    const allPages = useRef(0)
+
+    useEffect( () => {
+        getItems()
+    }, [itemsPerPage, currentPage])
+
+    const getItems = async () => {
+        setLoading(true);
+
+        const response = await fetch(`https://reqres.in/api/users?page=${currentPage}&per-page=${itemsPerPage}`)
+
+        const responseJson = await  response.json()
+
+        console.log(responseJson);
+    }
+
     return (
         <>
-        
+
         </>
     )
 }
